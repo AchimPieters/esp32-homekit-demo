49bf17ff60cf09e0a22176f77e3e5a5cf0fe0272
Achim Pieters <Achim.Pieters@outlook.com>
2024-05-31 19:27:25 +0200

    Optimization

---
+++ examples/lsc_smart_plug/README.md
@@ -0,0 +1,40 @@
+# Example for `LSC Smart Power Plug`
+
+## What it does
+
+It's a "Hello World" example for the HomeKit Demo. It is a code to turn ON and OFF an LED connected to an ESP Module.
+
+Brand: LSC (Smart Connect)
+Product: Smart Power Plug
+VERSION: 970762 | 970764 | 970766
+ART. No 2578685
+
+## Wiring
+
+Connect `LED` pin to the following pin:
+
+| Name | Description | Defaults |
+|------|-------------|----------|
+| `CONFIG_ESP_RELAY_GPIO` | GPIO number for `RELAY` pin | "18" Default |
+| `CONFIG_ESP_BUTTON_GPIO` | GPIO number for `BUTTON` pin | "10" Default |
+| `CONFIG_ESP_RED_LED_GPIO` | GPIO number for `RED_LED` pin | "7" Default |
+| `CONFIG_ESP_BLUE_LED_GPIO` | GPIO number for `BLUE_LED` pin | "4" Default |
+
+
+## Requirements
+
+Currently, a preview release is available, and can be installed like this:
+
+```
+idf.py add-dependency "wolfssl/wolfssl^5.7.1-preview2e"
+```
+
+To enable support for Apple HomeKit, be sure to check the respective box in `idf.py menuconfig`.
+
+The local `sdkconfig.defaults` has been edited to enable the wolfSSL support for Apple HomeKit by default.
+
+## Notes
+
+- Choose your GPIO number under `StudioPieters` in `menuconfig`. The default is `2` (On an ESP32 WROOM 32D).
+- Set your `WiFi SSID` and `WiFi Password` under `StudioPieters` in `menuconfig`.
+- Optional: You can change `HomeKit Setup Code` and `HomeKit Setup ID` under `StudioPieters` in `menuconfig`. (Note:  you need to make a new QR-CODE To make it work)
--- examples/lsc_smart_plug/main/main.c
+++ examples/lsc_smart_plug/main/main.c
@@ -34,58 +34,83 @@
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
 
-// Life Cycle Manager
-#include "lcm_api.h"
+#include <button.h>
 
 // WiFi setup
 void on_wifi_ready();
 
 static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-        if (event_base == WIFI_EVENT && (event_id == WIFI_EVENT_STA_START || event_id == WIFI_EVENT_STA_DISCONNECTED)) {
-                ESP_LOGI("WIFI_EVENT", "STA start");
-                esp_wifi_connect();
-        } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-                ESP_LOGI("IP_EVENT", "WiFI ready");
-                on_wifi_ready();
-        }
+    if (event_base == WIFI_EVENT && (event_id == WIFI_EVENT_STA_START || event_id == WIFI_EVENT_STA_DISCONNECTED)) {
+        ESP_LOGI("WIFI_EVENT", "STA start");
+        esp_wifi_connect();
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ESP_LOGI("IP_EVENT", "WiFI ready");
+        on_wifi_ready();
+    }
 }
 
 static void wifi_init() {
-        ESP_ERROR_CHECK(esp_netif_init());
-        ESP_ERROR_CHECK(esp_event_loop_create_default());
-        esp_netif_create_default_wifi_sta();
+    ESP_ERROR_CHECK(esp_netif_init());
+    ESP_ERROR_CHECK(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
-        ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-        ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    ESP_ERROR_CHECK(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
 
-        wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
-        ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
-        ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
+    ESP_ERROR_CHECK(esp_wifi_init(&wifi_init_config));
+    ESP_ERROR_CHECK(esp_wifi_set_storage(WIFI_STORAGE_RAM));
 
-        wifi_config_t wifi_config = {
-                .sta = {
-                        .ssid = CONFIG_ESP_WIFI_SSID,
-                        .password = CONFIG_ESP_WIFI_PASSWORD,
-                },
-        };
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_ESP_WIFI_SSID,
+            .password = CONFIG_ESP_WIFI_PASSWORD,
+        },
+    };
 
-        ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
-        ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-        ESP_ERROR_CHECK(esp_wifi_start());
+    ESP_ERROR_CHECK(esp_wifi_set_mode(WIFI_MODE_STA));
+    ESP_ERROR_CHECK(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    ESP_ERROR_CHECK(esp_wifi_start());
 }
 
+/**
+| `CONFIG_ESP_RELAY_GPIO` | GPIO number for `RELAY` pin | "18" Default |
+| `CONFIG_ESP_BUTTON_GPIO` | GPIO number for `BUTTON` pin | "10" Default |
+| `CONFIG_ESP_RED_LED_GPIO` | GPIO number for `RED_LED` pin | "7" Default |
+| `CONFIG_ESP_BLUE_LED_GPIO` | GPIO number for `BLUE_LED` pin | "4" Default |
+**/
+
+
+#define BUTTON_GPIO CONFIG_ESP_BUTTON_GPIO
+
 // LED control
-#define LED_GPIO CONFIG_ESP_LED_GPIO
+#define LED_GPIO CONFIG_ESP_BLUE_LED_GPIO
 bool led_on = false;
 
 void led_write(bool on) {
         gpio_set_level(LED_GPIO, on ? 1 : 0);
 }
-// All GPIO Settings
+
+#define RELAY_GPIO CONFIG_ESP_RELAY_GPIO
+
+void relay_write(bool on) {
+        gpio_set_level(RELAY_GPIO, on ? 1 : 0);
+}
+void switch_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context);
+
+homekit_characteristic_t switch_on = HOMEKIT_CHARACTERISTIC_(
+        ON, false, .callback=HOMEKIT_CHARACTERISTIC_CALLBACK(switch_on_callback)
+        );
+
 void gpio_init() {
         gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
         led_write(led_on);
+
+        gpio_set_direction(RELAY_GPIO, GPIO_MODE_OUTPUT);
+        relay_write(switch_on.value.bool_value);
 }
+
+
 
 // Accessory identification
 void accessory_identify_task(void *args) {
@@ -106,27 +131,40 @@ void accessory_identify(homekit_value_t _value) {
         ESP_LOGI("ACCESSORY_IDENTIFY", "Accessory identify");
         xTaskCreate(accessory_identify_task, "Accessory identify", 2048, NULL, 2, NULL);
 }
+// Accessory identification
 
-
-homekit_value_t led_on_get() {
-        return HOMEKIT_BOOL(led_on);
+// switch
+void switch_on_callback(homekit_characteristic_t *_ch, homekit_value_t on, void *context) {
+        relay_write(switch_on.value.bool_value);
 }
 
-void led_on_set(homekit_value_t value) {
-        if (value.format != homekit_format_bool) {
-                ESP_LOGE("led_on_set", "Invalid value format: %d", value.format);
-                return;
+void button_callback(button_event_t event, void *context) {
+        switch (event) {
+        case button_event_single_press:
+                ESP_LOGI("SINGLE_PRESS", "single press");
+                switch_on.value.bool_value = !switch_on.value.bool_value;
+                relay_write(switch_on.value.bool_value);
+                homekit_characteristic_notify(&switch_on, switch_on.value);
+                break;
+        case button_event_double_press:
+                ESP_LOGI("DOUBLE_PRESS", "Double press");
+
+                break;
+        case button_event_long_press:
+                ESP_LOGI("LONG_PRESS", "Long press");
+
+                break;
+        default:
+                ESP_LOGI("UNKNOWN_BUTTON_EVENT", "unknown button event: %d", event);
         }
-        led_on = value.bool_value;
-        led_write(led_on);
 }
 
-// HomeKit characteristics
-#define DEVICE_NAME "HomeKit LED"
+
+#define DEVICE_NAME "HomeKit Switch"
 #define DEVICE_MANUFACTURER "StudioPieters®"
 #define DEVICE_SERIAL "NLDA4SQN1466"
 #define DEVICE_MODEL "SD466NL/A"
-#define FW_VERSION "0.0.0"
+#define FW_VERSION "0.0.1"
 
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, DEVICE_NAME);
 homekit_characteristic_t manufacturer = HOMEKIT_CHARACTERISTIC_(MANUFACTURER,  DEVICE_MANUFACTURER);
@@ -138,7 +176,7 @@ homekit_characteristic_t revision = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISION, F
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Woverride-init"
 homekit_accessory_t *accessories[] = {
-        HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_lighting, .services = (homekit_service_t*[]) {
+        HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_switches, .services = (homekit_service_t*[]) {
                 HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics = (homekit_characteristic_t*[]) {
                         &name,
                         &manufacturer,
@@ -148,10 +186,9 @@ homekit_accessory_t *accessories[] = {
                         HOMEKIT_CHARACTERISTIC(IDENTIFY, accessory_identify),
                         NULL
                 }),
-                HOMEKIT_SERVICE(LIGHTBULB, .primary = true, .characteristics = (homekit_characteristic_t*[]) {
-                        HOMEKIT_CHARACTERISTIC(NAME, "HomeKit LED"),
-                        HOMEKIT_CHARACTERISTIC(ON, false, .getter = led_on_get, .setter = led_on_set),
-                        NULL
+                HOMEKIT_SERVICE(SWITCH, .primary = true, .characteristics = (homekit_characteristic_t*[]) {
+                        HOMEKIT_CHARACTERISTIC(NAME, "HomeKit Switch"),
+                        &switch_on,
                 }),
                 NULL
         }),
@@ -165,7 +202,9 @@ homekit_server_config_t config = {
         .setupId = CONFIG_ESP_SETUP_ID,
 };
 
-void on_wifi_ready() {}
+void on_wifi_ready() {
+        homekit_server_init(&config);
+}
 
 void app_main(void) {
         esp_err_t ret = nvs_flash_init();
@@ -177,4 +216,17 @@ void app_main(void) {
 
         wifi_init();
         gpio_init();
+
+        #pragma GCC diagnostic push
+        #pragma GCC diagnostic ignored "-Woverride-init"
+        button_config_t button_config = BUTTON_CONFIG(
+                button_active_low,
+                .max_repeat_presses=2,
+                .long_press_time=1000,
+                );
+        #pragma GCC diagnostic pop
+
+        if (button_create(BUTTON_GPIO, button_config, button_callback, NULL)) {
+                ESP_LOGI("FAILED_TO_INITIALIZE_BUTTON", "Failed to initialize button");
+        }
 }
