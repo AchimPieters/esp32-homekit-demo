b49464b5452f132d759fd5de57c7250008263252
Achim Pieters <Info@studiopieters.nl>
2024-12-13 20:39:44 +0100

    Update DHT API
    Update DHT API

--- examples/temperature_sensor/main/main.c
+++ examples/temperature_sensor/main/main.c
@@ -18,7 +18,7 @@
    WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE, ARISING FROM, OUT OF OR IN
    CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN THE SOFTWARE.
 
-   for more information visit https://www.studiopieters.nl
+   For more information, visit https://www.studiopieters.nl
  **/
 
 #include <stdio.h>
@@ -31,72 +31,72 @@
 #include <driver/gpio.h>
 #include <homekit/homekit.h>
 #include <homekit/characteristics.h>
-
+#include <math.h> // Include for fabs
 #include <dht.h>
 
-// Custom error handling macro
-#define CHECK_ERROR(x) do {                        \
-                esp_err_t __err_rc = (x);                  \
-                if (__err_rc != ESP_OK) {                  \
+#define CHECK_ERROR(x) do {                          \
+                esp_err_t __err_rc = (x);            \
+                if (__err_rc != ESP_OK) {            \
                         ESP_LOGE("INFORMATION", "Error: %s", esp_err_to_name(__err_rc)); \
-                        handle_error(__err_rc);                \
-                }                                          \
+                        handle_error(__err_rc);      \
+                }                                    \
 } while(0)
 
 void handle_error(esp_err_t err) {
-        switch (err) {
-        case ESP_ERR_WIFI_NOT_STARTED:
-        case ESP_ERR_WIFI_CONN:
-                ESP_LOGI("INFORMATION", "Restarting WiFi...");
-                esp_wifi_stop();
-                esp_wifi_start();
-                break;
-        default:
-                ESP_LOGE("ERROR", "Critical error, restarting device...");
-                esp_restart();
-                break;
-        }
+    switch (err) {
+    case ESP_ERR_WIFI_NOT_STARTED:
+    case ESP_ERR_WIFI_CONN:
+        ESP_LOGI("INFORMATION", "Restarting WiFi...");
+        esp_wifi_stop();
+        esp_wifi_start();
+        break;
+    default:
+        ESP_LOGE("ERROR", "Critical error, restarting device...");
+        esp_restart();
+        break;
+    }
 }
 
 static void on_wifi_ready();
 
 static void event_handler(void *arg, esp_event_base_t event_base, int32_t event_id, void *event_data) {
-        if (event_base == WIFI_EVENT) {
-                if (event_id == WIFI_EVENT_STA_START || event_id == WIFI_EVENT_STA_DISCONNECTED) {
-                        ESP_LOGI("INFORMATION", "Connecting to WiFi...");
-                        esp_wifi_connect();
-                }
-        } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
-                ESP_LOGI("INFORMATION", "WiFi connected, IP obtained");
-                on_wifi_ready();
+    if (event_base == WIFI_EVENT) {
+        if (event_id == WIFI_EVENT_STA_START || event_id == WIFI_EVENT_STA_DISCONNECTED) {
+            ESP_LOGI("INFORMATION", "Connecting to WiFi...");
+            esp_wifi_connect();
         }
+    } else if (event_base == IP_EVENT && event_id == IP_EVENT_STA_GOT_IP) {
+        ESP_LOGI("INFORMATION", "WiFi connected, IP obtained");
+        on_wifi_ready();
+    }
 }
 
 static void wifi_init() {
-        CHECK_ERROR(esp_netif_init());
-        CHECK_ERROR(esp_event_loop_create_default());
-        esp_netif_create_default_wifi_sta();
+    CHECK_ERROR(esp_netif_init());
+    CHECK_ERROR(esp_event_loop_create_default());
+    esp_netif_create_default_wifi_sta();
 
-        CHECK_ERROR(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
-        CHECK_ERROR(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
+    CHECK_ERROR(esp_event_handler_register(WIFI_EVENT, ESP_EVENT_ANY_ID, &event_handler, NULL));
+    CHECK_ERROR(esp_event_handler_register(IP_EVENT, IP_EVENT_STA_GOT_IP, &event_handler, NULL));
 
-        wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
-        CHECK_ERROR(esp_wifi_init(&wifi_init_config));
-        CHECK_ERROR(esp_wifi_set_storage(WIFI_STORAGE_RAM));
+    wifi_init_config_t wifi_init_config = WIFI_INIT_CONFIG_DEFAULT();
+    CHECK_ERROR(esp_wifi_init(&wifi_init_config));
+    CHECK_ERROR(esp_wifi_set_storage(WIFI_STORAGE_RAM));
 
-        wifi_config_t wifi_config = {
-                .sta = {
-                        .ssid = CONFIG_ESP_WIFI_SSID,
-                        .password = CONFIG_ESP_WIFI_PASSWORD,
-                        .threshold.authmode = WIFI_AUTH_WPA2_PSK,
-                },
-        };
+    wifi_config_t wifi_config = {
+        .sta = {
+            .ssid = CONFIG_ESP_WIFI_SSID,
+            .password = CONFIG_ESP_WIFI_PASSWORD,
+            .threshold.authmode = WIFI_AUTH_WPA2_PSK,
+        },
+    };
 
-        CHECK_ERROR(esp_wifi_set_mode(WIFI_MODE_STA));
-        CHECK_ERROR(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
-        CHECK_ERROR(esp_wifi_start());
+    CHECK_ERROR(esp_wifi_set_mode(WIFI_MODE_STA));
+    CHECK_ERROR(esp_wifi_set_config(ESP_IF_WIFI_STA, &wifi_config));
+    CHECK_ERROR(esp_wifi_start());
 }
 
+// Sensor type definitions
 #if defined(CONFIG_EXAMPLE_TYPE_DHT11)
 #define SENSOR_TYPE DHT_TYPE_DHT11
 #endif
@@ -107,73 +107,89 @@ static void wifi_init() {
 #define SENSOR_TYPE DHT_TYPE_SI7021
 #endif
 
-// LED control
 #define LED_GPIO CONFIG_ESP_LED_GPIO
 static bool led_on = false;
 
 void led_write(bool on) {
-        gpio_set_level(LED_GPIO, on ? 1 : 0);
+    gpio_set_level(LED_GPIO, on ? 1 : 0);
 }
 
 void gpio_init() {
-        gpio_reset_pin(LED_GPIO);
-        gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
-        led_write(led_on);
+    gpio_reset_pin(LED_GPIO);
+    gpio_set_direction(LED_GPIO, GPIO_MODE_OUTPUT);
+    led_write(led_on);
 }
 
 void accessory_identify_task(void *args) {
-        for (int i = 0; i < 3; i++) {
-                for (int j = 0; j < 2; j++) {
-                        led_write(true);
-                        vTaskDelay(pdMS_TO_TICKS(100));
-                        led_write(false);
-                        vTaskDelay(pdMS_TO_TICKS(100));
-                }
-                vTaskDelay(pdMS_TO_TICKS(250));
+    for (int i = 0; i < 3; i++) {
+        for (int j = 0; j < 2; j++) {
+            led_write(true);
+            vTaskDelay(pdMS_TO_TICKS(100));
+            led_write(false);
+            vTaskDelay(pdMS_TO_TICKS(100));
         }
-        led_write(led_on);
-        vTaskDelete(NULL);
+        vTaskDelay(pdMS_TO_TICKS(250));
+    }
+    led_write(led_on);
+    vTaskDelete(NULL);
 }
 
 void accessory_identify(homekit_value_t _value) {
-        ESP_LOGI("INFORMATION", "Accessory identify");
-        xTaskCreate(accessory_identify_task, "Accessory identify", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
+    ESP_LOGI("INFORMATION", "Accessory identify");
+    xTaskCreate(accessory_identify_task, "Accessory identify", configMINIMAL_STACK_SIZE, NULL, 2, NULL);
 }
 
 homekit_characteristic_t temperature = HOMEKIT_CHARACTERISTIC_(CURRENT_TEMPERATURE, 0);
-homekit_characteristic_t humidity    = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
+homekit_characteristic_t humidity = HOMEKIT_CHARACTERISTIC_(CURRENT_RELATIVE_HUMIDITY, 0);
 
 void temperature_sensor_task(void *pvParameters) {
-        float temperature_value, humidity_value;
+    float temperature_value, humidity_value;
+    float last_temperature = -100.0;
+    float last_humidity = -100.0;
+    const float temp_threshold = 0.5;
+    const float hum_threshold = 1.0;
+    const int periodic_interval = 1800;
+    int elapsed_time = 0;
 
 #ifdef CONFIG_EXAMPLE_INTERNAL_PULLUP
-        gpio_set_pull_mode(CONFIG_ESP_TEMP_SENSOR_GPIO, GPIO_PULLUP_ONLY);
+    gpio_set_pull_mode(CONFIG_ESP_TEMP_SENSOR_GPIO, GPIO_PULLUP_ONLY);
 #endif
 
-        while (1) {
-                if (dht_read_float_data(SENSOR_TYPE, CONFIG_ESP_TEMP_SENSOR_GPIO, &humidity_value, &temperature_value) == ESP_OK) {
-                        ESP_LOGI("INFORMATION", "Humidity: %.1f%% Temp: %.1fC", humidity_value, temperature_value);
-                        temperature.value.float_value = temperature_value;
-                        humidity.value.float_value = humidity_value;
-                        homekit_characteristic_notify(&temperature, HOMEKIT_FLOAT(temperature_value));
-                        homekit_characteristic_notify(&humidity, HOMEKIT_FLOAT(humidity_value));
-                } else {
-                        ESP_LOGE("ERROR", "Cannot read data from sensor");
-                }
-                vTaskDelay(pdMS_TO_TICKS(2000));
+    while (1) {
+        if (dht_read_float_data(SENSOR_TYPE, CONFIG_ESP_TEMP_SENSOR_GPIO, &humidity_value, &temperature_value) == ESP_OK) {
+            bool should_update_temp = fabs(temperature_value - last_temperature) >= temp_threshold;
+            bool should_update_hum = fabs(humidity_value - last_humidity) >= hum_threshold;
+
+            if (should_update_temp || should_update_hum || elapsed_time >= periodic_interval) {
+                ESP_LOGI("INFORMATION", "Updating values - Humidity: %.1f%%, Temp: %.1f°C", humidity_value, temperature_value);
+
+                temperature.value.float_value = temperature_value;
+                humidity.value.float_value = humidity_value;
+                homekit_characteristic_notify(&temperature, HOMEKIT_FLOAT(temperature_value));
+                homekit_characteristic_notify(&humidity, HOMEKIT_FLOAT(humidity_value));
+
+                last_temperature = temperature_value;
+                last_humidity = humidity_value;
+                elapsed_time = 0;
+            }
+        } else {
+            ESP_LOGE("ERROR", "Cannot read data from sensor");
         }
+
+        vTaskDelay(pdMS_TO_TICKS(10000));
+        elapsed_time += 10;
+    }
 }
 
 void temperature_sensor_init() {
-        xTaskCreate(temperature_sensor_task, "Temperature Sensor Task", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
+    xTaskCreate(temperature_sensor_task, "Temperature Sensor Task", configMINIMAL_STACK_SIZE * 3, NULL, 5, NULL);
 }
-
-// HomeKit characteristics
- #define DEVICE_NAME "HomeKit Temperature Sensor"
- #define DEVICE_MANUFACTURER "StudioPieters®"
- #define DEVICE_SERIAL "NLDA4SQN1466"
- #define DEVICE_MODEL "SD466NL/A"
- #define FW_VERSION "0.0.1"
+// Define device characteristics
+#define DEVICE_NAME "Temperature Sensor"
+#define DEVICE_MANUFACTURER "StudioPieters®"
+#define DEVICE_SERIAL "NLDA4SQN1466"
+#define DEVICE_MODEL "SD466NL/A"
+#define FW_VERSION "0.0.1"
 
 homekit_characteristic_t name = HOMEKIT_CHARACTERISTIC_(NAME, DEVICE_NAME);
 homekit_characteristic_t manufacturer = HOMEKIT_CHARACTERISTIC_(MANUFACTURER, DEVICE_MANUFACTURER);
@@ -184,53 +200,53 @@ homekit_characteristic_t revision = HOMEKIT_CHARACTERISTIC_(FIRMWARE_REVISION, F
 #pragma GCC diagnostic push
 #pragma GCC diagnostic ignored "-Woverride-init"
 homekit_accessory_t *accessories[] = {
-        HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_sensors, .services = (homekit_service_t*[]) {
-                HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics = (homekit_characteristic_t*[]) {
-                        &name,
-                        &manufacturer,
-                        &serial,
-                        &model,
-                        &revision,
-                        HOMEKIT_CHARACTERISTIC(IDENTIFY, accessory_identify),
-                        NULL
-                }),
-                HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
-                        HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
-                        &temperature,
-                        NULL
-                }),
-                HOMEKIT_SERVICE(HUMIDITY_SENSOR, .characteristics=(homekit_characteristic_t*[]) {
-                        HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
-                        &humidity,
-                        NULL
-                }),
-                NULL
+    HOMEKIT_ACCESSORY(.id = 1, .category = homekit_accessory_category_sensors, .services = (homekit_service_t*[]) {
+        HOMEKIT_SERVICE(ACCESSORY_INFORMATION, .characteristics = (homekit_characteristic_t*[]) {
+            &name,
+            &manufacturer,
+            &serial,
+            &model,
+            &revision,
+            HOMEKIT_CHARACTERISTIC(IDENTIFY, accessory_identify),
+            NULL
+        }),
+        HOMEKIT_SERVICE(TEMPERATURE_SENSOR, .primary=true, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Temperature Sensor"),
+            &temperature,
+            NULL
+        }),
+        HOMEKIT_SERVICE(HUMIDITY_SENSOR, .characteristics=(homekit_characteristic_t*[]) {
+            HOMEKIT_CHARACTERISTIC(NAME, "Humidity Sensor"),
+            &humidity,
+            NULL
         }),
         NULL
+    }),
+    NULL
 };
 #pragma GCC diagnostic pop
 
 homekit_server_config_t config = {
-        .accessories = accessories,
-        .password = CONFIG_ESP_SETUP_CODE,
-        .setupId = CONFIG_ESP_SETUP_ID,
+    .accessories = accessories,
+    .password = CONFIG_ESP_SETUP_CODE,
+    .setupId = CONFIG_ESP_SETUP_ID,
 };
 
 static void on_wifi_ready() {
-        ESP_LOGI("INFORMATION", "Starting HomeKit server...");
-        homekit_server_init(&config);
+    ESP_LOGI("INFORMATION", "Starting HomeKit server...");
+    homekit_server_init(&config);
 }
 
 void app_main(void) {
-        esp_err_t ret = nvs_flash_init();
-        if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
-                ESP_LOGW("WARNING", "NVS flash initialization failed, erasing...");
-                CHECK_ERROR(nvs_flash_erase());
-                ret = nvs_flash_init();
-        }
-        CHECK_ERROR(ret);
+    esp_err_t ret = nvs_flash_init();
+    if (ret == ESP_ERR_NVS_NO_FREE_PAGES || ret == ESP_ERR_NVS_NEW_VERSION_FOUND) {
+        ESP_LOGW("WARNING", "NVS flash initialization failed, erasing...");
+        CHECK_ERROR(nvs_flash_erase());
+        ret = nvs_flash_init();
+    }
+    CHECK_ERROR(ret);
 
-        wifi_init();
-        gpio_init();
-        temperature_sensor_init();
+    wifi_init();
+    gpio_init();
+    temperature_sensor_init();
 }
