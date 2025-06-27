08f57ba19cf8a2e28a98b9f401599c81b49dcd3e
Achim Pieters <Achim.Pieters@outlook.com>
2025-06-27 18:37:26 +0200

    Revert "Revert "Add HSI to RGBW conversion unit test""

---
+++ components/hsi2rgbw/hsi2rgbw.c
@@ -0,0 +1,39 @@
+#include "hsi2rgbw.h"
+#include <math.h>
+
+void hsi2rgbw(float H, float S, float I, int *rgbw) {
+    int r = 0, g = 0, b = 0, w = 0;
+    float cos_h, cos_1047_h;
+
+    H = fmodf(H, 360.0f);
+    H = (float)M_PI * H / 180.0f;
+    S = fminf(fmaxf(S, 0.0f), 1.0f);
+    I = fminf(fmaxf(I, 0.0f), 1.0f);
+
+    if (H < 2.09439f) {
+        cos_h = cosf(H);
+        cos_1047_h = cosf(1.047196667f - H);
+        r = S * 255 * I / 3 * (1 + cos_h / cos_1047_h);
+        g = S * 255 * I / 3 * (1 + (1 - cos_h / cos_1047_h));
+        w = 255 * (1 - S) * I;
+    } else if (H < 4.188787f) {
+        H -= 2.09439f;
+        cos_h = cosf(H);
+        cos_1047_h = cosf(1.047196667f - H);
+        g = S * 255 * I / 3 * (1 + cos_h / cos_1047_h);
+        b = S * 255 * I / 3 * (1 + (1 - cos_h / cos_1047_h));
+        w = 255 * (1 - S) * I;
+    } else {
+        H -= 4.188787f;
+        cos_h = cosf(H);
+        cos_1047_h = cosf(1.047196667f - H);
+        b = S * 255 * I / 3 * (1 + cos_h / cos_1047_h);
+        r = S * 255 * I / 3 * (1 + (1 - cos_h / cos_1047_h));
+        w = 255 * (1 - S) * I;
+    }
+
+    rgbw[0] = r;
+    rgbw[1] = g;
+    rgbw[2] = b;
+    rgbw[3] = w;
+}
